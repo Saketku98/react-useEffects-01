47100692ee9f8f21b76b3ece50cb123bc766b39b
Saketku98 <88224770+Saketku98@users.noreply.github.com>
2023-08-20 22:40:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Downtown from "./Components/Downtown";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Downtown />
+      </header>
+    </div>
+  );
+}
+
+export default App;
